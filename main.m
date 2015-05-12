d56ee4824eab65a36f1f5f03f02d9fc797027c53
Josephine <jossan_cuellar@hotmail.com>
2015-05-12 10:12:14 +0200

    ..

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clc
 clear all
 
 % Read image of simple road
-I = imread('Bild2.png');
+I = imread('Bild4.png');
 
 % % Show original image
 figure(1)
@@ -12,14 +12,14 @@ imshow(I)
 title('Original image')
 
 % Cut the image
-IR=im2double(cutImage(I(:,:,1)));
-IG=im2double(cutImage(I(:,:,2)));
-IB=im2double(cutImage(I(:,:,3)));
+IR = im2double(cutImage(I(:,:,1)));
+IG = im2double(cutImage(I(:,:,2)));
+IB = im2double(cutImage(I(:,:,3)));
 
 % Threshold for the RGB-images
-IR_thres = IR > getThreshold(IR, 0.5);
-IG_thres = IG > getThreshold(IG, 0.5);
-IB_thres = IB > getThreshold(IB, 0.5);
+IR_thres = IR > getThreshold(IR, 0.40);
+IG_thres = IG > getThreshold(IG, 0.40);
+IB_thres = IB > getThreshold(IB, 0.40);
 
 % Convert I to a hsv-image and threshold the saturated image
 Ihsv = rgb2hsv(I);
@@ -27,37 +27,32 @@ IS = cutImage(Ihsv(:,:,2));
 IS_threshold = getThreshold(IS,0.3);
 IS = IS < IS_threshold; % Good pic to extract the road from!
 
-IV = cutImage(Ihsv(:,:,3));
-IV_threshold = getThreshold(IV,0.85);
-IV = IV > IV_threshold; % Doesn't give too much info
-
-% Sum all images up to get the best image. Works good for 'Bild2' to reduce
-%the reflections from the water
-% I_best = IB_thres+IR_thres+IG_thres+IS+IV;
-% I_best = I_best > 4;
-
 % Sum all images up to get the best image
 I_best = IB_thres+IR_thres+IG_thres+IS;
 I_best = I_best > 3;
 
+% Removed noise
+InoNoiseRoad = imcomplement(bwareaopen(imcomplement(I_best),300));
+InoNoiseR = bwareaopen(InoNoiseRoad, 1000);
 
 % Find white lines
 
-% Threshold for the RGB-images
+% Threshold for the RGB-images. In Bild2, use 98%
 IR_thres = IR > getThreshold(IR, 0.9);
 IG_thres = IG > getThreshold(IG, 0.9);
 IB_thres2 = IB > getThreshold(IB, 0.9);
 
 % Sum all images up to get the best image
 I_bestLines = IB_thres2+IR_thres+IG_thres+IS;
-I_bestLines = imcomplement(I_bestLines > 3);
+I_bestLines = I_bestLines > 3;
 
-% Removed noise from actual road
-InoNoiseRoad=imcomplement(bwareaopen(imcomplement(I_best),100));
-InoNoise=bwareaopen(InoNoiseRoad, 1000);
-IroadLines=InoNoise-I_bestLines;
-IroadLinesNoNoise=imcomplement(bwareaopen(imcomplement(IroadLines), 100));
+% Removed noise
+InoNoiseLines = imcomplement(bwareaopen(imcomplement(I_bestLines),300));
+InoNoiseL = bwareaopen(InoNoiseLines, 200);
+
+% Subtract lines from road
+IroadLines = InoNoiseR-InoNoiseL;
 
 figure(2)
 subplot(1,1,1)
-imshow(IroadLinesNoNoise)
+imshow(IroadLines)
